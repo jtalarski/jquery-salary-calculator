0c1af8f1c8106dce80d3b256bfceb98801bbc33f
Jim Talarski <jim.talarski@gmail.com>
2020-08-29 12:36:16 -0500

    pass new record and display

--- notes.md
+++ notes.md
@@ -2,8 +2,8 @@
 [X] add submit button
 [X] build array to take in object of employee info
 [X] Build function to take in new record and place in array
-[ ] empty input fields
 [ ] Append employee information to the DOM
+[ ] empty input fields
 [ ] calculate monthly costs and append this to DOM
 [ ] Create delete button that removes employee from DOM
 
--- script.js
+++ script.js
@@ -23,5 +23,27 @@ function addEmployee() {
     }; // end newRecord
     employeeInfo.push(newRecord);
     console.log(employeeInfo);
-
+    // pass employeeInfo to DOM
+    displayEmployees();
 } // end addEmployee
+
+function displayEmployees() {
+    console.log('in displayEmployees');
+    // mess with outputArea
+    let el = $('#outputArea');
+    el.empty();
+    // loop through employeeInfo to display contents
+    for (let i = 0; i < employeeInfo.length; i++) {
+        el.append(`
+        <li>
+        ${employeeInfo[i].first}
+        ${employeeInfo[i].last}
+        ${employeeInfo[i].id}
+        ${employeeInfo[i].title}
+        ${employeeInfo[i].salary}
+        </li>
+        `);
+        el.empty();
+    } // end for
+
+} // end displayEmployees
