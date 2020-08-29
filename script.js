8c5585f2b91ae9f9b9332f1f02f9054d09ff3c43
Jim Talarski <jim.talarski@gmail.com>
2020-08-29 16:33:14 -0500

    formatted numbers as psuedo currency

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function onReady() {
     console.log('I am ready');
 
     //$('.addEmployeeBtn').on('click', addEmployee);
-    $(document).on("click", ".addEmployeeBtn", addEmployee);
+    $(document).on("click", ".addEmployeeBtn", addEmployee); {}
     $(document).on('click', '.deleteButton', onDelete);
 
 }
@@ -30,6 +30,7 @@ function addEmployee() {
     console.log(employeeInfo);
     // pass employeeInfo to DOM
     displayEmployees();
+    //displayEmployeeTable();
     calculateMonthly();
 } // end addEmployee
 
@@ -37,6 +38,7 @@ function displayEmployees() {
     console.log('in displayEmployees');
     // mess with outputArea
     let el = $('#outputArea');
+    el.empty();
     el.empty();
     // loop through employeeInfo to display contents
     for (let i = 0; i < employeeInfo.length; i++) {
@@ -53,6 +55,24 @@ function displayEmployees() {
     } // end for
 } // end displayEmployees
 
+/*function displayEmployeeTable() {
+    console.log('in displayEmployeeTable');
+    let el = $('#tableAttempt');
+    el.empty();
+    for (let i = 0; i < employeeInfo.length; i++) {
+        el.append(`
+        <tr>
+            <td>${employeeInfo[i].first}</td>
+            <td>${employeeInfo[i].last}</td>
+            <td>${employeeInfo[i].id}</td>
+            <td>${employeeInfo[i].title}</td>
+            <td>A${employeeInfo[i].salary}</td>
+            <td><button class=deleteButton>Delete</button></td>
+        </tr>
+        `);
+    } //end for
+}*/
+
 function onDelete() {
     console.log('in onDelete', $(this));
     $(this).parent().remove();
@@ -63,10 +83,20 @@ function calculateMonthly() {
     let totalYearly = 0
     for (let i = 0; i < employeeInfo.length; i++) {
         totalYearly += Number(employeeInfo[i].salary);
-        totalMonthly = totalYearly / 12
+        totalMonthly = totalYearly / 12;
+        console.log('$', totalMonthly.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 }))
     } // end for
     console.log("monthly total", totalYearly, totalMonthly);
     let el = $('#showMonthly');
     el.empty();
-    el.append('Total Monthly:', totalMonthly)
+    el.append(
+        "Total Monthly: $",
+        totalMonthly.toLocaleString(undefined, {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+        })
+    );
+    if (totalMonthly >= 20000) {
+        $('#showMonthly').css('background-color', 'red');
+    }
 } //end calculateMonthly
